59b60555ffd22727348397ff3286e7fc9663e904
David Hromádka <hromadka.david@student.alej.cz>
2024-12-10 20:41:10 +0100

    Added Doxygen comments to GeoCurve

--- src/geoObjects/geoCurve.cpp
+++ src/geoObjects/geoCurve.cpp
@@ -1,11 +1,19 @@
 #include "geoCurve.h"
 
+/// @brief Basic constructor of curve
+/// @param parent DrawingCanvas on which the object is
+/// @param name Name of the object
+/// @param type Type of the curve
 GeoCurve::GeoCurve(wxWindow *parent, wxString &name, GeoCurveType type)
     : GeoObject(parent, name){
     
     this->type = type;
 }
 
+/// @brief Returns the distance of a point from the curve
+/// @details Distance from given point and the closest point on the curve returned from GetClosestPoint()
+/// @param pt Point from which the distance is calculated
+/// @return The distance
 double GeoCurve::GetDistance(const wxPoint2DDouble &pt) {
     return pt.GetDistance(this->GetClosestPoint(pt));
 }
--- src/geoObjects/geoCurve.h
+++ src/geoObjects/geoCurve.h
@@ -4,26 +4,53 @@
 
 #include "geoObject.h"
 
+/// All possible types of a curve
 enum GeoCurveType {
     LINE,
     SEGMENT
 };
 
+/// @brief Abstract class for a curve
 class GeoCurve : public GeoObject {
 public:
     GeoCurve(wxWindow *parent, wxString &name, GeoCurveType type);
 
+    /// Returns the type of the curve
     GeoCurveType GetType() {return type;}
 
+    
+    /// @brief Calculates the perpendicular projection of \p pt on the curve
+    /// @param pt The point to project
+    /// @return The projected point
     virtual wxPoint2DDouble GetPerpPoint(const wxPoint2DDouble &pt){ return GetClosestPoint(pt); }
+
+    /// @brief Calculates the closest point on the curve to \p pt
+    /// @param pt The point from which the distance is taken
+    /// @return The closest point
+    /// @note This can have different behaviour from GetPerpPoint() on curves that are only part of a curve (such as segment). The function GetPerpPoint() "extends" the curve and gets the perpendicular projection. GetClosestPoint() returns the actual closest point. 
     virtual wxPoint2DDouble GetClosestPoint(const wxPoint2DDouble &pt) = 0;
     double GetDistance(const wxPoint2DDouble &pt);
 
+
+    /// @brief Returns a parameter of a point on the curve.
+    /// @param pt The point on the curve
+    /// @return The parameter of \p pt
+    /// @warning Point must be on the curve
     virtual double GetParameter(const wxPoint2DDouble &pt) = 0;
+
+    /// @brief Calculates point on the curve from a given parameter
+    /// @param param The given parameter
+    /// @return The calculated point
+    /// @warning Parameter must be valid (ideally a value returned from GetParameter())
     virtual wxPoint2DDouble GetPointFromParameter(const double &param) = 0;
 
+    /// @brief Calculates the tangent vector to the curve at point
+    /// @param pt Point at which the tangent is calculated
+    /// @return The tangent vector at \p pt
+    /// @warning Point must be on the curve
     virtual wxPoint2DDouble GetTangentAtPoint(const wxPoint2DDouble &pt) = 0;
 
 private:
+    /// Type of the curve
     GeoCurveType type;
 };
